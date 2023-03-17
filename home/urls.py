0c15ab3afe869c1836e6cf144e45cd322efc7867
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-03-17 13:55:36 +0200

    adaugare robots.txt

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .views import home, about, portofolio, info, rezervare
 from django.contrib.sitemaps.views import sitemap
 from .sitemaps import StaticViewSitemap
@@ -13,5 +13,6 @@ urlpatterns = [
     path("portofoliu/", portofolio, name="portofoliu"),
     path("info/", info, name="info"),
     path("rezervare/", rezervare, name="rezervare"),
-    path("sitemap.xml", sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
+    path("sitemap.xml", sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path("robots.txt", include("robots.urls")),
 ]
